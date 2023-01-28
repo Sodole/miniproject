1a759ea45b9d121db95b605ff91a60e500d5535f
Sodole <lws0915@gmail.com>
2023-01-29 00:44:53 +0900

    로그인 오류

--- omorobot/urls.py
+++ omorobot/urls.py
@@ -7,14 +7,14 @@ app_name = "omorobot"
 urlpatterns = [
     path("", landing, name="landing"),
     path("signin/", signin, name="signin"),
+    path("signin/read_user/", read_user, name="signin"),
     path("signup/", signup, name="signup"),
+    path("signup/createuser/", create_user, name="create_user"),
     path("detail/<str:pk>/", detail, name="detail"),
     path("input/<str:pk>/createmycar/", create_mycar, name="create_mycar"),
     path("input/<str:pk>/", input, name="input"),
     path("record/<str:pk>/",record, name="record"),
     path("realtime/<str:pk>/", realtime, name="realtime"),
     path("index/<str:pk>/", index, name="index"),  
-    path("createuser/", create_user, name="create_user"),
-    path("read_user/", read_user, name="signin"),
     path("deleteuser/<str:pk>/", delete_user, name="delete_user"),
 ]
