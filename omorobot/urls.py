980a63d013bbfbe032c7892874f6f4f65bc869f9
Sodole <lws0915@gmail.com>
2023-01-29 00:17:16 +0900

    input page 완성

--- omorobot/urls.py
+++ omorobot/urls.py
@@ -9,10 +9,12 @@ urlpatterns = [
     path("signin/", signin, name="signin"),
     path("signup/", signup, name="signup"),
     path("detail/<str:pk>/", detail, name="detail"),
+    path("input/<str:pk>/createmycar/", create_mycar, name="create_mycar"),
     path("input/<str:pk>/", input, name="input"),
     path("record/<str:pk>/",record, name="record"),
     path("realtime/<str:pk>/", realtime, name="realtime"),
     path("index/<str:pk>/", index, name="index"),  
     path("createuser/", create_user, name="create_user"),
     path("read_user/", read_user, name="signin"),
+    path("deleteuser/<str:pk>/", delete_user, name="delete_user"),
 ]
--- omorobot/views.py
+++ omorobot/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import User
+from .models import User, Mycar
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -7,8 +7,10 @@ context = {
     "signupmessage" : "",
     "signinmessage" : ""
 }
+inputmessage ={
+    "errormessage" :"asdf"}
 
-# Create your views here.
+## 페이지 로직
 def landing(request):
     return render(request, "landing.html")
 
@@ -27,78 +29,110 @@ def signup(request):
     return render(request, "signup.html", context)
 
 def index(request, pk):
+    global inputmessage
+    inputmessage = {
+            "errormessage" :""}
+    mycar = Mycar.objects.all()
     if User.objects.filter(user_name = f"{pk}").exists() :
         old_user = User.objects.get(user_name=f"{pk}")
         old_name = old_user.user_name
-        context = {
-            "User" : old_name
+        allcontext = {
+            "context" : {"User" : old_name, "mycars": mycar}
         }
-        return render(request, "index.html", context)
+        return render(request, "index.html", allcontext)
     elif pk == "main" :
-        context = {
-            "User" : "main"
+        allcontext = {
+            "context" : {"User" : "main"}
         }
-        return render(request, "index.html", context)
+        return render(request, "index.html", allcontext)
 
 def detail(request, pk):
+    global inputmessage
+    inputmessage = {
+            "errormessage" :""}
     if User.objects.filter(user_name = f"{pk}").exists() :
         old_user = User.objects.get(user_name=f"{pk}")
         old_name = old_user.user_name
-        context = {
-            "User" : old_name
+        allcontext = {
+            "context" : {"User" : old_name}
         }
-        return render(request, "detail.html", context)
+        return render(request, "detail.html", allcontext)
     elif pk == "main" :
-        context = {
-            "User" : "main"
+        allcontext = {
+            "context" : {"User" : "main"}
         }
-        return render(request, "detail.html", context)
+        return render(request, "detail.html", allcontext)
 
 def input(request, pk):
+    global inputmessage
     if User.objects.filter(user_name = f"{pk}").exists() :
         old_user = User.objects.get(user_name=f"{pk}")
         old_name = old_user.user_name
-        context = {
-            "User" : old_name
+        allcontext = {
+            "context" : {"User" : old_name},
+            "text" : inputmessage
         }
-        return render(request, "input.html", context)
+
+        return render(request, "input.html", allcontext)
     elif pk == "main" :
-        context = {
-            "User" : "main"
-        }
-        return render(request, "input.html", context)
+        allcontext = {
+            "context" : {"User" : "main"},
+            "text" : inputmessage
+         }
+        return render(request, "input.html", allcontext)
 
 
 def record(request, pk):
+    global inputmessage
+    inputmessage = {
+            "errormessage" :""}
     if User.objects.filter(user_name = f"{pk}").exists() :
         old_user = User.objects.get(user_name=f"{pk}")
         old_name = old_user.user_name
-        context = {
-            "User" : old_name
+        allcontext = {
+            "context" : {"User" : old_name}
         }
-        return render(request, "record.html", context)
+        return render(request, "record.html", allcontext)
     elif pk == "main" :
-        context = {
-            "User" : "main"
+        allcontext = {
+            "context" : {"User" : "main"}
         }
-        return render(request, "record.html", context)
+        return render(request, "record.html", allcontext)
 
 def realtime(request, pk):
+    global inputmessage
+    inputmessage = {
+            "errormessage" :""}
     if User.objects.filter(user_name = f"{pk}").exists() :
         old_user = User.objects.get(user_name=f"{pk}")
         old_name = old_user.user_name
-        context = {
-            "User" : old_name
+        allcontext = {
+            "context" : {"User" : old_name}
         }
-        return render(request, "realtime.html", context)
+        return render(request, "realtime.html", allcontext)
     elif pk == "main" :
-        context = {
-            "User" : "main"
+        allcontext = {
+            "context" : {"User" : "main"}
         }
-        return render(request, "realtime.html", context)
+        return render(request, "realtime.html", allcontext)
 
 
-# 회원가입로직
+## user관리
+def delete_user(request, pk):
+    del_user = User.objects.filter(pk=pk)
+    del_user.delete()
+    return HttpResponseRedirect(reverse("omorobot:index"))
+
+def update_user(request, pk) :
+  if request.method == "POST" :
+    user_model = request.POST.get("model")
+    old_user = User.objects.get(pk=pk)
+    old_user.models = user_model
+    old_user.save()
+    return HttpResponseRedirect(reverse("omorobot:index"))
+
+
+## 회원가입로직
 def create_user(request) :
     global context
     if request.method == "POST":
@@ -185,3 +219,51 @@ def read_user(request) :
                     return HttpResponseRedirect(reverse("omorobot:signin"))
     else :
         return render(request, "signin.html", context)
+
+
+# mycar 관리
+def create_mycar(request, pk):
+    input_speed = request.GET.get("speed")
+    input_battery = request.GET.get("battery")
+    input_color = request.GET.get("color")
+    global inputmessage
+    print(input_speed, input_battery, input_color)
+    if User.objects.filter(user_name = f"{pk}").exists() :
+        new_mycar = Mycar()
+        try :
+            new_mycar.mycar_speed = int(input_speed)
+            new_mycar.mycar_battery = int(input_battery)
+            new_mycar.mycar_color = input_color
+            new_mycar.user_name = User.objects.get(user_name=f"{pk}")
+            new_mycar.save()
+            inputmessage = {
+            "errormessage" : "생성되었습니다"
+            }
+            return HttpResponseRedirect(f"/input/{pk}/")
+        except :
+            inputmessage = {
+            "errormessage" : "올바른 값을하세요"
+        }
+            return HttpResponseRedirect(f"/input/{pk}/")
+    else : 
+        inputmessage = {
+        "errormessage" : "로그인을해주세요"
+        }
+        return HttpResponseRedirect(f"/input/{pk}/")
+
+def delete_mycar(request, pk):
+    del_mycar = Mycar.objects.filter(pk=pk)
+    del_mycar.delete()
+    return HttpResponseRedirect(reverse("omorobot:index"))
+
+def update_mycar(request, pk) :
+  if request.method == "POST" :
+    mycar_speed = request.POST.get("speed")
+    mycar_battery = request.POST.get("battery")
+    mycar_color = request.POST.get("color")
+    old_minicar = Mycar.objects.get(pk=pk)
+    old_minicar.speed = mycar_speed
+    old_minicar.battery = mycar_battery
+    old_minicar.color = mycar_color
+    old_minicar.save()
+    return HttpResponseRedirect(reverse("omorobot:index"))
